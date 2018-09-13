e634b36c77fbdd03b152700f5d3bc0b5994068d3
milafilip <milafilipovwork1@gmail.com>
2018-09-13 19:17:43 +0100

    render a cube

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,41 @@ import * as THREE from "three";
 
 class App extends React.Component {
   private container;
+  private camera;
+  private renderer;
 
   componentDidMount() {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
+    this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xcccccc);
-    this.container.appendChild(renderer.domElement);
-    renderer.render(scene, camera);
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true
+    });
+    this.renderer.setClearColor(0xcccccc);
+    this.container.appendChild(this.renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: "red" });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    this.camera.position.set(3, 3, 3);
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    this.renderer.render(scene, this.camera);
+
+    window.addEventListener("resize", this.handleResize.bind(this));
+    this.handleResize.bind(this);
+  }
+
+  handleResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
   render() {
