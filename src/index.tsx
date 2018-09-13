79756bdef2488368b8a8346c348da3d59f1a9032
milafilip <milafilipovwork1@gmail.com>
2018-09-13 19:11:27 +0100

    full width height and width renderer

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,27 @@
 import * as React from "react";
 import { render } from "react-dom";
+import * as THREE from "three";
 
-function App() {
-  return <h1>APP</h1>;
+class App extends React.Component {
+  private container;
+
+  componentDidMount() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xcccccc);
+    this.container.appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+  }
+
+  render() {
+    return <div ref={el => (this.container = el)} />;
+  }
 }
 
 render(<App />, document.getElementById("app"));
